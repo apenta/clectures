557ea0084e7aa2f4bad695989f27e09e0bad131f
Josh Tucholski <josh@techelevator.com>
2018-01-16 22:51:17 -0500

    Added day 3 lecture code.

---
+++ m1-w1d3-expressions-lecture/Lecture.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MorningExercises.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MorningExercises.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f81f66df-615c-4896-ad87-b46874377dda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ m1-w1d3-expressions-lecture/Lecture/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*** IF STATEMENTS ***");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+
+            bool staplerIsSwingline = false;
+            if (staplerIsSwingline)
+            {
+                Console.WriteLine("Better be red...");
+            }
+
+
+
+            int numberOfPeople = 25;
+            int slicesOfCake = 22;
+
+            if (numberOfPeople <= slicesOfCake)
+            {
+                Console.WriteLine("Yummy! Cake!");
+            }
+            else
+            {
+                Console.WriteLine("Burn the building down...");
+            }
+
+
+           
+
+
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*** VARIABLE SCOPE          ***");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+
+            int firstVariable = 2;
+            if (firstVariable > 0)
+            {
+                int secondVariable = firstVariable;
+            }
+            //int thirdVariable = secondVariable * 2; // this will cause a compile error because secondVariable is not "in scope"
+            //int firstVariable = 3;  // this is a compiler error because we already have a variable named firstVariable in this scope
+
+
+            Console.WriteLine("*******************************");
+            Console.WriteLine("********** METHODS ************");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            bool burnItDown = ShouldBurnDownTheBuilding(14, 20);
+
+            Console.WriteLine("Value of burnItDown: " + burnItDown);
+        }
+
+        static bool ShouldBurnDownTheBuilding(int numberOfEmployees, int piecesOfCake)
+        {
+            if (numberOfEmployees > piecesOfCake)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
