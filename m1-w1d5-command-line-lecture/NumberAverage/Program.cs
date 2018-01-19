796e70a7018bde6552512ca62e80d26c7678e741
Josh Tucholski <josh@techelevator.com>
2018-01-19 11:44:43 -0500

    Added lecture code for week 1 day 5.

---
+++ m1-w1d5-command-line-lecture/NumberAverage/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.Write("Hello, what is your name?: ");
+            string name = Console.ReadLine();
+
+            //Console.WriteLine("Hello " + name); 
+            Console.WriteLine($"Hello {name}");
+
+            Console.Write($"Ok {name}, how many numbers do you want to average?: ");
+
+            // Prompt the user for a number
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            // int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for(int i = 1; i <= number; i++)
+            {
+                Console.Write($"Enter number {i} ");
+
+                input = Console.ReadLine();
+                sum += int.Parse(input);
+
+                
+            }
+
+            double average = (double)sum / number;
+            Console.WriteLine($"The average is {average}");
+        }
+    }
+}
