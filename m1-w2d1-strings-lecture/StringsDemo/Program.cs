f981fea1098f80edc53c87749cd0faab164cc726
Josh Tucholski <josh@techelevator.com>
2018-01-22 11:38:58 -0500

    Added lecture code.

--- m1-w2d1-strings-lecture/StringsDemo/Program.cs
+++ m1-w2d1-strings-lecture/StringsDemo/Program.cs
@@ -13,9 +13,6 @@ namespace StringsDemo
             string name = "Ada Lovelace";
 
 
-
-
-
             // Strings are actually arrays of characters (char). 
             // Those characters can be accessed using [] notation.
 
@@ -24,47 +21,87 @@ namespace StringsDemo
             // Output: A
             // Output: e
 
-            // Console.WriteLine("First and Last Character. ");
+            char firstCharacter = name[0]; // first character
+            char lastCharacter = name[name.Length - 1]; // last character
+
+            Console.WriteLine(firstCharacter + lastCharacter); // <-- does not concatenate value together
+                                                                                     // instead it adds the ASCII values
+
+            char capitalA = (char)101; //we can cast decimal into a character
+
+
+            Console.WriteLine("First Character: " + firstCharacter);
+            Console.WriteLine("Last Character: " + lastCharacter);
 
             // 2. How do we write code that prints out the first three characters
             // Output: Ada
 
-            // Console.WriteLine("First 3 characters: ");
+            string firstThreeCharacters = name.Substring(0, 3);
+
+            Console.WriteLine("First 3 characters: " + firstThreeCharacters);
 
             // 3. Now print out the first three and the last three characters
             // Output: Adaace
 
-            // Console.WriteLine("Last 3 characters: ");
+            string lastThreeCharacters = name.Substring(name.Length - 3);       // start 3 characters from the end
+
+            Console.WriteLine("Last 3 characters: " + firstThreeCharacters + lastThreeCharacters);
 
             // 4. What about the last word?
             // Output: Lovelace
 
-            // Console.WriteLine("Last Word: ");
+            string[] words = name.Split(' '); // split name at each space
+            //string[] words = name.Split(' ', '?', '.', '!') // splits on all forms of punctuation and spaces
+
+            Console.WriteLine("Last Word: " + words[words.Length - 1]);
 
 
             // 5. Does the string contain inside of it "Love"?
             // Output: true
 
-            // Console.WriteLine("Contains \"Love\"");
+            bool containsLove = name.Contains("Love");
+
+            Console.WriteLine("Contains \"Love\": " + containsLove);
 
             // 6. Where does the string "lace" show up in name?
             // Output: 8
 
-            // Console.WriteLine("Index of \"lace\": ");
+            int indexOfLace = name.IndexOf("lace");
+
+            Console.WriteLine("Index of \"lace\": " + indexOfLace);
 
             // 7. How many 'a's OR 'A's are in name?
             // Output: 3
 
-            // Console.WriteLine("Number of \"a's\": ");
+            int sum = 0;
+            for(int i = 0; i < name.Length; i++)
+            {
+                if (name[i] == 'a' || name[i] == 'A')
+                {
+                    sum += 1;
+                }
+            }
 
-            // 8. Replace "Ada Lovelance" with "Ada, Countess of Lovelace"
+            Console.WriteLine("Number of \"a's\": " + sum);
 
-            // Console.WriteLine(name);
+            // 8. Replace "Ada Lovelace" with "Ada, Countess of Lovelace"
+            name = name.Replace("Ada Lovelace", "Ada, Countess of Lovelace");
+            Console.WriteLine(name);
+
 
             // 9. Set name equal to null.
+            name = null;
 
             // 10. If name is equal to null or "", print out "All Done".
-
+            if (name == null || name.Length == 0)
+            {
+                Console.WriteLine("All done");
+            }
+            // These two are the same way of writing "if name is null or empty string"
+            if (String.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("All done");
+            }
 
             
         }
