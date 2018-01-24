edd59c849cc96e490ac2ed2c7f20ee51af76306f
Josh Tucholski <josh@techelevator.com>
2018-01-24 06:22:04 -0500

    Added extra step for lecture code.

--- m1-w2d3-collections-part2-lecture/DictionaryCollection/Program.cs
+++ m1-w2d3-collections-part2-lecture/DictionaryCollection/Program.cs
@@ -27,14 +27,30 @@ namespace DictionaryCollection
                 Console.Write("What is the person's name?: ");
                 string name = Console.ReadLine();
 
-                Console.Write("What is the person's height?: ");
+                Console.Write("What is the person's height (in inches)?: ");
                 int height = int.Parse(Console.ReadLine());
 
-                // 2. Put the name and height into the dictionary
-                //      dictionaryVariable[key] = value;
-                //      OR dictionaryVariable.Add(key, value);
+                // 2. Check to see if that name is in the dictionary
+                //      bool exists = dictionaryVariable.ContainsKey(key)
+                bool exists = false;    // <-- change this
+
+                if (!exists)
+                {
+                    Console.WriteLine($"Adding {name} with new value.");
+                    // 3. Put the name and height into the dictionary
+                    //      dictionaryVariable[key] = value;
+                    //      OR dictionaryVariable.Add(key, value);
+
+                }
+                else
+                {
+                    Console.WriteLine($"Overwriting {name} with new value.");
+                    // 4. Overwrite the current key with a new value
+                    //      dictionaryVariable[key] = value;
+                }
 
 
+                Console.WriteLine();
                 Console.Write("Would you like to enter another person (yes/no)? ");
                 input = Console.ReadLine().ToLower();
             }
@@ -47,22 +63,26 @@ namespace DictionaryCollection
                 Console.Write("Which name are you looking for? ");
                 input = Console.ReadLine();
 
-                //3. Let's print a specific name from the dictionary
+                //5. Let's get a specific name from the dictionary
 
 
             }
             else if (input == "all")
-            {                
+            {
                 Console.WriteLine();
                 Console.WriteLine(".... printing ...");
 
-                //4. Let's print each item in the dictionary
-
+                //6. Let's print each item in the dictionary
 
             }
+
+            Console.WriteLine();
+            Console.WriteLine("Done...");
+
+            //7. Let's get the average height of the people in the dictionary
         }
 
-        static void PrintDictionary(Dictionary<string, int> dictionary)
+        static void PrintDictionary(Dictionary<string, int> database)
         {
             // Looping through a dictionary involves using a foreach loop
             // to look at each item, which is a key-value pair
