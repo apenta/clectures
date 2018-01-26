594f2e0159f88ddffd62cd86731e47b8c8d40ff4
Josh Tucholski <josh@techelevator.com>
2018-01-26 12:07:15 -0500

    Added encapsulation lecture code.

---
+++ m1-w2d5-encapsulation/DeckOfCards/Program.cs
@@ -0,0 +1,23 @@
+﻿using DeckOfCards.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            for (int i = 1; i <= 52; i++)
+            {
+                Card topCard = deck.DealOne();
+                Console.WriteLine($"{topCard.FaceValue} of {topCard.Suit} - {topCard.Symbol}");
+            }
+        }
+    }
+}
