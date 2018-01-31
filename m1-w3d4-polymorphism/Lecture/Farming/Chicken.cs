df531c0620308d7cb8c1612d88c983fdcc45e086
Josh Tucholski <josh@techelevator.com>
2018-01-30 21:22:37 -0500

    Added polymorphism lecture code.

---
+++ m1-w3d4-polymorphism/Lecture/Farming/Chicken.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture.Farming
+{
+    public class Chicken
+    {        
+        public string NameOfAnimal
+        {
+            get
+            {
+                return "Chicken";
+            }
+        }      
+
+        public string MakeSoundOnce()
+        {
+            return "Cluck";
+        }
+
+        public string MakeSoundTwice()
+        {
+            return "Cluck Cluck";
+        }
+
+        // Kind of a neat trick you may see the above written like this below
+        // http://bit.ly/2nui9HV
+        // This is called "Expression-Bodied Members" and its new in C# (2015).
+        // You can write a single expression in place of the return code.
+        /*
+        public string NameOfAnimal
+        {
+            get => "Chicken";
+        }
+        public string MakeSoundOnce() => "Cluck";
+        public string MakeSoundTwice() => "Cluck Cluck";
+        */
+
+    }
+}
