2fe09744eeff787831cdedd7197f46d47c8e92a4
Josh Tucholski <josh@techelevator.com>
2018-02-02 08:54:00 -0500

    Added oop review starter code.

---
+++ m1-w3d5-oop-review/CardGames/Cards/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGames.Cards
+{
+    /// <summary>
+    /// Represents a "card".
+    /// </summary>
+    public abstract class Card
+    {
+        /// <summary>
+        /// If the card is facing up.
+        /// </summary>
+        public bool IsFaceUp { get; private set; }
+
+        /// <summary>
+        /// Background color of our card.
+        /// </summary>
+        public virtual ConsoleColor BackgroundColor { get; protected set; } = ConsoleColor.White;
+
+        /// <summary>
+        /// Foreground color of our card.
+        /// </summary>
+        public virtual ConsoleColor ForegroundColor { get; protected set; } = ConsoleColor.Black;
+
+
+        /// <summary>
+        /// Flips the card.
+        /// </summary>
+        public void Flip()
+        {
+            IsFaceUp = !IsFaceUp;
+        }
+
+        /// <summary>
+        /// Prints the value on the card.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetDisplayValue();
+
+
+    }
+}
---
+++ m1-w3d5-oop-review/CardGames/Decks/Deck.cs
@@ -0,0 +1,42 @@
+﻿using CardGames.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGames.Decks
+{
+    /// <summary>
+    /// Represents a "deck"
+    /// </summary>
+    public abstract class Deck
+    {
+        /// <summary>
+        /// The cards in the deck.
+        /// IList<T> is used because we need to add, remove, clear, etc. our list.
+        /// </summary>
+        protected IList<Card> Cards { get; }
+
+        /// <summary>
+        /// A property that is derived and returns TRUE if count > 0.
+        /// </summary>
+        public bool HasCards { get => Cards.Count > 0; }
+
+
+        /// <summary>
+        /// Creates a new deck.
+        /// </summary>
+        public Deck()
+        {
+            Cards = BuildDeck();
+        }
+
+        /// <summary>
+        /// Abstract method relies on subclasses to "build the deck".
+        /// </summary>
+        /// <returns></returns>
+        protected abstract IList<Card> BuildDeck();
+
+    }
+}
