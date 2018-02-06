437a3cee7a5fcb3f773791852ff2de730d2f988c
Josh Tucholski <josh@techelevator.com>
2018-02-06 07:12:17 -0500

    Added file io part 1 lecture.

---
+++ m1-w4d2-file-io-part1-lecture/ExceptionHandling/Exceptions/OverdraftException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Exceptions
+{
+    public class OverdraftException : Exception
+    {
+
+        public OverdraftException() : base() { }
+        public OverdraftException(string message) : base(message) { }
+        public OverdraftException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ m1-w4d2-file-io-part1-lecture/Lecture/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lecture.Aids;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example Code, located in Aids folder
+            //ReadingInFiles.ReadACharacterFile();
+            //ReadingCSVFiles.ReadFile();
+            //SummingUpNumbers.ReadFile();         
+
+            // Let's read in a file
+            //
+            //  ABC-1234|Shadow|Lab|Black|12/2/2016
+            //  DEF-5678|Sprinkles|Yorkshire Terrier|Brown|6/5/2015
+            //  GHI-9012|Neo|Golden Retriever|Red|6/1/2013
+
+
+
+        }
+    }
+}
