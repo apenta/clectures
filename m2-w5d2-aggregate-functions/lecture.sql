ced782afc1db9c6079faf43a84e57dd98638e989
Josh Tucholski <josh@techelevator.com>
2018-02-13 06:50:30 -0500

    Pushed lecture code for aggregate functions.

---
+++ m2-w5d2-aggregate-functions/lecture.sql
@@ -0,0 +1,65 @@
+-- ORDERING RESULTS
+
+-- Populations of all countries in descending order
+
+
+--Names of countries and continents in ascending order
+
+
+-- Cities in Ascending Order, Population in Descending Order
+
+
+-- LIMITING RESULTS
+-- The name and average life expectancy of the countries with the 10 highest life expectancies.
+
+
+
+
+-- CONCATENATING OUTPUTS
+
+-- The name & state of all cities in California, Oregon, or Washington. 
+-- "city, state", sorted by state then city
+
+
+
+
+
+-- AGGREGATE FUNCTIONS
+-- Average Life Expectancy in the World
+
+-- Total population in Ohio
+
+-- The surface area of the smallest country in the world
+
+-- The 10 largest countries in the world
+
+
+-- The number of countries who declared independence in 1991
+
+
+
+-- GROUP BY EXERCISES
+-- GROUP BY clusters rows by a column value
+
+-- Count the number of countries where each language is spoken, order show them from most countries to least
+
+-- Average life expectancy of each continent ordered from highest to lowest
+
+-- Exclude Antarctica from consideration for average life expectancy
+
+-- Sum of the population of cities in each state in the USA ordered by state name
+
+-- The average population of cities in each state in the USA ordered by state name
+
+-- The minimum population and the maximum population of each country code
+
+
+-- Additional samples
+-- You may alias column and table names to provide more descriptive names
+
+-- Alias can also be used to create shorthand references, such c for city and co for country.
+
+
+
+
+
