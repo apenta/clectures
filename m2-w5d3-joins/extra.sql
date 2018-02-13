114a365938e6a7f6de83bae1083c2fb44df5cf8b
Josh Tucholski <josh@techelevator.com>
2018-02-13 16:22:56 -0500

    Added databases and joins lecture code.

---
+++ m2-w5d3-joins/extra.sql
@@ -0,0 +1,27 @@
+-- Display all the films and their language
+
+-- Display all the films and in English
+
+
+-- Display all the films with their category
+
+
+-- Display all the films with a category of Horror
+
+
+-- Display all the films with a category of Horror and title that begins with the letter F
+
+
+
+
+-- Who acted in what together? (SQL SERVER)
+
+
+-- How many times have two actors appeared together? (POSTGRESQL)
+
+
+-- How many times have two actors appeared together? (SQL SERVER)
+
+
+-- What movies did the two most often acted together actors appear in together?
+
