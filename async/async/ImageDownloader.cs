9d83ba2c460f9b7e2330aeb5148bdb5422cff42c
Josh Tucholski <josh@techelevator.com>
2018-02-13 16:03:53 -0500

    Added async lecture code.

---
+++ async/async/ImageDownloader.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace async
+{
+    public class ImageDownloader
+    {
+        private string folder;
+
+        public ImageDownloader(string folder)
+        {
+            this.folder = folder;            
+        }
+
+        /// <summary>
+        /// Downloads a random photo synchronously
+        /// </summary>
+        public void DownloadRandomPhoto()
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            try
+            {
+                WebClient client = new WebClient();
+
+                // DownloadFile takes two arguments, a URI and a final file name.
+                client.DownloadFile(new Uri($"https://picsum.photos/800/1200/?random"), $"{folder}/{Guid.NewGuid()}.jpg");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return;
+        }        
+
+
+
+
+        /// <summary>
+        /// Downloads a random image asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        public async Task DownloadRandomPhotoAsync(int i)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            try
+            {
+                WebClient client = new WebClient();
+
+                // DownloadFileTaskAsync takes two arguments, a URI and a final file name.
+                await client.DownloadFileTaskAsync(new Uri($"https://picsum.photos/800/1200/?random"), $"{folder}/{Guid.NewGuid()}.jpg");
+
+                Console.WriteLine("Finished download - " + i );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return;
+        }
+
+
+        /// <summary>
+        /// Downloads a random photo and gets the image file size synchronously
+        /// </summary>
+        /// <returns></returns>
+        public int GetRandomPhotoSize()
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+                // DownloadData takes one arguments, a URI
+                byte[] file = client.DownloadData(new Uri($"https://picsum.photos/200/300/?random"));
+
+                return file.Length;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Downloads a random photo asynchronously and gets the image size.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<int> GetRandomPhotoSizeAsync()
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+                // DownloadData takes one arguments, a URI
+                byte[] file = await client.DownloadDataTaskAsync(new Uri($"https://picsum.photos/200/300/?random"));
+
+                return file.Length;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ async/async/Program.cs
@@ -0,0 +1,209 @@
+﻿using System.IO;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            // This code will download a series of images synchronously, one after the other.
+            //SyncDownloads();
+
+            Console.ReadLine();
+
+            // This method will instruct the program to download the images asynchronously.
+            // The method won't be able to finish until all images have been downloaded.                        
+            //AsyncDownloads().Wait();
+            
+            MassiveAsyncDownloads().Wait();
+        }
+
+        /// <summary>
+        /// We'll use the stopwatch to track how long this method takes to complete.
+        /// </summary>
+        private static void SyncDownloads()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            ImageDownloader downloader = new ImageDownloader("sync");
+            int i = 0;
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            downloader.DownloadRandomPhoto();
+
+            sw.Stop();
+
+            Console.WriteLine($"Downloaded {i} images in {sw.ElapsedMilliseconds}ms");
+        }
+        
+        private async static Task MassiveAsyncDownloads()
+        {
+            List<Task> tasks = new List<Task>(); //all of the ongoing tasks
+            ImageDownloader downloader = new ImageDownloader("lotsoffiles");
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int i = 0;
+
+            while (i < 1_000)
+            {
+                Console.WriteLine($"Downloading image {i}");
+                tasks.Add(downloader.DownloadRandomPhotoAsync(i));
+
+                i++;
+            }
+
+            Task.WaitAll(tasks.ToArray()); //wait for all tasks to complete before proceeding forward
+
+            sw.Stop();
+            Console.WriteLine($"Downloaded {i} images in {sw.ElapsedMilliseconds}ms");
+
+        }
+
+
+        /// <summary>
+        /// We'll send the requests asynchronously but measure the time it takes on the stopwatch for all of them to complete.
+        /// </summary>
+        /// <returns>Task - so that our void main can wait for it to complete.</returns>
+        private async static Task AsyncDownloads()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            ImageDownloader downloader = new ImageDownloader("async");
+            int i = 0;
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task1 = downloader.DownloadRandomPhotoAsync(i);
+            
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task2 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task3 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task4 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task5 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task6 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task7 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task8 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task9 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task10 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task11 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task12 = downloader.DownloadRandomPhotoAsync(i);
+
+            i++;
+            Console.WriteLine($"Downloading image {i}");
+            var task13 = downloader.DownloadRandomPhotoAsync(i);
+
+            await task1;
+            await task2;
+            await task3;
+            await task4;
+            await task5;
+            await task6;
+            await task7;
+            await task8;
+            await task9;
+            await task10;
+            await task11;
+            await task12;
+            await task13;
+
+            sw.Stop();
+
+            Console.WriteLine($"Downloaded {i} images in {sw.ElapsedMilliseconds}ms");
+        }                
+    }
+}
