39fd118f8b6c597204c119bf5548da29708be710
Josh Tucholski <josh@techelevator.com>
2018-02-14 11:48:46 -0500

    Added lecture code for joins.

--- m2-w5d3-joins/lecture.sql
+++ m2-w5d3-joins/lecture.sql
@@ -1,31 +1,99 @@
 -- ********* INNER JOIN ***********
 
 -- Let's find out who made payment 16666:
+SELECT * 
+FROM payment
+WHERE payment.payment_id = 16666;
 
 
 -- Ok, that gives us a customer_id, but not the name. We can use the customer_id to get the name FROM the customer table
+SELECT * 
+FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment.payment_id = 16666;
+
 
 
 -- We can see that the * pulls back everything from both tables. We just want everything from payment and then the first and last name of the customer:
+SELECT payment.*, customer.first_name, customer.last_name 
+FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment.payment_id = 16666;
+
+
 
 
 -- But when did they return the rental? Where would that data come from? From the rental table, so let’s join that.
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date
+FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id
+JOIN rental ON payment.rental_id = rental.rental_id
+WHERE payment.payment_id = 16666;
+
+
 
 
 -- What did they rent? Film id can be gotten through inventory.
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date, film.title
+FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id
+JOIN rental ON payment.rental_id = rental.rental_id
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+WHERE payment.payment_id = 16666;
 
 
--- What if we wanted to know who acted in that film?    
+
+
+
+
+
+-- What if we wanted to know who acted in that film? 
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date, film.title, actor.first_name, actor.last_name
+FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id
+JOIN rental ON payment.rental_id = rental.rental_id
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE payment.payment_id = 16666;
+
+
+
+
+
+
+
+
+   
 
 
 -- What if we wanted a list of all the films and their categories ordered by film title
+SELECT film.title, category.name
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON category.category_id = film_category.category_id
+ORDER BY film.title;
+
+
 
 
 -- Show all the 'Comedy' films ordered by film title
+SELECT film.title, category.name
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON category.category_id = film_category.category_id
+WHERE category.name = 'Comedy'
+ORDER BY film.title;
 
 
 -- Finally, let's count the number of films under each category
-
+SELECT category.name, COUNT(*) as numberOfFilms
+FROM category
+JOIN film_category ON category.category_id = film_category.category_id
+GROUP BY category.name
+ORDER BY numberOfFilms DESC;
 
 
 -- ********* LEFT JOIN ***********
@@ -35,6 +103,13 @@
 -- A Left join, selects all records from the "left" table and matches them with records from the "right" table if a matching record exists. 
 
 -- Let's display a list of all countries and their capitals, if they have some.
+SELECT country.name, country.capital, city.id, city.name
+FROM country
+JOIN city ON country.capital = city.id;
+
+SELECT * FROM country WHERE capital IS NULL;
+
+
 
 
 -- Only 232 rows
@@ -43,6 +118,11 @@
 
 -- There are 239 countries. So how do we show them all even if they don’t have a capital?
 -- That’s because if the rows don’t exist in both tables, we won’t show any information for it. If we want to show data FROM the left side table everytime, we can use a different join:
+SELECT country.name, country.capital, city.id, city.name
+FROM country
+RIGHT JOIN city ON country.capital = city.id;
+
+
 
 
 -- *********** UNION *************
@@ -50,6 +130,12 @@
 -- Back to the "dvdstore" database...
 
 -- Gathers a list of all first names used by actors and customers
+SELECT actor.first_name, actor.last_name, 'A' as 'type' FROM actor
+UNION
+SELECT customer.first_name, '', 'C' FROM customer;
+
+
+
 -- By default removes duplicates
 
 
