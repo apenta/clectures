762a5b01c76c9980d64455e6d3babf87a8a19c80
Josh Tucholski <josh@techelevator.com>
2018-02-19 08:45:16 -0500

    Pushed data access lecture code.

---
+++ m2-w6d1-data-access-layers/WorldGeography/DAL/CitySqlDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class CitySqlDAL
+    {
+        private string connectionString;
+
+        public CitySqlDAL(string databaseconnectionString)
+        {
+            connectionString = databaseconnectionString;
+        }
+
+
+        public List<City> GetCitiesByCountryCode(string countryCode)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
