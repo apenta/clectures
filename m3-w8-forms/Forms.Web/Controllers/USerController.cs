a6e47c1652a0bbc81d597f378dc4960429c49fc9
Josh Tucholski <josh@techelevator.com>
2018-03-06 11:00:32 -0500

    Pushed lecture code.

---
+++ m3-w8-forms/Forms.Web/Controllers/USerController.cs
@@ -0,0 +1,32 @@
+﻿using Forms.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forms.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //4. Add a Post Action to Process Registration
+        [HttpPost]
+        public ActionResult Registration(UserRegistrationModel model)
+        {
+            // code that calls DAL
+            return RedirectToAction("Success"); //5. Redirect user to /User/Success
+        }
+
+        //6. Add a User/Success action
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ m3-w8-forms/Forms.Web/Models/UserRegistrationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forms.Web.Models
+{
+    /* 1. Add a model for our input page */
+    public class UserRegistrationModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
